1b0222ec656baada8737951528c712a7aa1f403b
Yozorakumo <74314351+yozorakumo@users.noreply.github.com>
2026-04-02 13:25:50 +0900

    Fix: Correctly access transmissionType from carParams using fpsm in frogpilot_annotated_camera.cc

--- frogpilot/ui/qt/onroad/frogpilot_annotated_camera.cc
+++ frogpilot/ui/qt/onroad/frogpilot_annotated_camera.cc
@@ -258,7 +258,7 @@ void FrogPilotAnnotatedCameraWidget::paintFrogPilotWidgets(QPainter &p, UIState 
     paintWeather(p, frogpilotPlan, frogpilot_scene);
   }
 
-  if (s.scene.car_params.getTransmissionType() == cereal::CarParams::TransmissionType::MANUAL || frogpilot_toggles.value("MazdaMTUI").toBool()) {
+  if (fpsm["carParams"].getCarParams().getTransmissionType() == cereal::CarParams::TransmissionType::MANUAL || frogpilot_toggles.value("MazdaMTUI").toBool()) {
     paintMTGear(p, carState, frogpilotCarState);
   }
 }
