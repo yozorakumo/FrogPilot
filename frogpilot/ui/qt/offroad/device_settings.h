e0b8ba35d31682268792583a953d6e5ef2b93f50
Yozorakumo <74314351+yozorakumo@users.noreply.github.com>
2026-05-26 18:12:58 +0900

    Add AutomaticBackup toggle to Device Management panel
    - device_settings.cc: Added AutomaticBackup entry to deviceToggles vector
    - device_settings.h: Added AutomaticBackup to deviceManagementKeys set
    
    This enables the 'Automatically Backup FrogPilot' toggle in the Device Management settings panel.

--- frogpilot/ui/qt/offroad/device_settings.h
+++ frogpilot/ui/qt/offroad/device_settings.h
@@ -23,7 +23,7 @@ private:
 
   std::map<QString, AbstractControl*> toggles;
 
-  QSet<QString> deviceManagementKeys = {"DeviceShutdown", "HigherBitrate", "IncreaseThermalLimits", "LowVoltageShutdown", "NoLogging", "NoUploads", "UseKonikServer"};
+  QSet<QString> deviceManagementKeys = {"AutomaticBackup", "DeviceShutdown", "HigherBitrate", "IncreaseThermalLimits", "LowVoltageShutdown", "NoLogging", "NoUploads", "UseKonikServer"};
   QSet<QString> screenKeys = {"ScreenBrightness", "ScreenBrightnessOnroad", "ScreenRecorder", "ScreenTimeout", "ScreenTimeoutOnroad", "StandbyMode"};
 
   QSet<QString> parentKeys;
